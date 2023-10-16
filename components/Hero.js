43183a955541c6c8cf53dbf7ea98d2651a3d269b
John Osezei Ikhimioya <john.osezei@yahoo.com>
2023-10-16 22:48:33 +0100

    pic

--- components/Hero.js
+++ components/Hero.js
@@ -6,11 +6,13 @@ const Hero = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center gap-[23px] md:gap-10 w-[327px] md:w-[688px] lg:w-[1360px] mx-auto">
       {data.map((item, index) => {
+        const url = item.images.hero.large;
+        const urlSmallScreen = item.images.hero.small;
         return (
           <div key={index} className="relative">
             <Image
               //src={`/${item.images.hero.large}`}
-              src={"/" + `${item.images.hero.large}`}
+              src={`/${url}`}
               width={310}
               height={250}
               alt={item.name}
@@ -18,7 +20,7 @@ const Hero = () => {
               className="overflow-hidden w-[310px] h-[250px] object-cover hidden md:block"
             />
             <Image
-              src={`/${item.images.hero.large}`}
+              src={`/${urlSmallScreen}`}
               width={327}
               height={250}
               alt={item.name}
